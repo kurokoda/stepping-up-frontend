da960e1a278c054dc09c9c4a113a81e9db1267fe
Kurokoda <indigo.developer@gmail.com>
2018-02-22 23:16:06 -0500

    Adding firebase communication

--- src/_web/views/about/index.js
+++ src/_web/views/about/index.js
@@ -1,11 +1,33 @@
 import React, {Component} from 'react';
 import {Button} from 'react-bootstrap';
 import {withRouter} from 'react-router-dom';
+import firebase from '../../../shared/firebase';
 
 class AboutView extends Component {
   static propTypes = {};
 
   static defaultProps = {};
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: []
+    };
+  }
+
+  componentWillMount() {
+    let messages = firebase.database().ref('messages').orderByKey().limitToLast(100);
+    messages.on('child_added', snapshot => {
+      let message = {text: snapshot.val(), id: snapshot.key};
+      this.setState({messages: [message].concat(this.state.messages)});
+    })
+  }
+
+  onSubmit = (event) => {
+    firebase.database().ref('messages').push(this.input.value);
+    this.input.value = '';
+    event.preventDefault();
+  }
 
   render() {
     const buttonTitle = 'Go To Home';
@@ -13,6 +35,15 @@ class AboutView extends Component {
 
     return (
       <section style={styles.container}>
+        <form onSubmit={this.onSubmit}>
+          <input type="text" ref={ el => this.input = el }/>
+          <input type="submit"/>
+          <ul>
+            {
+              this.state.messages.map(message => <li key={message.id}>{message.text}</li>)
+            }
+          </ul>
+        </form>
         <div style={styles.content}>
           <span style={styles.text}>{textString}</span>
           <Button onClick={this.onClick}>{buttonTitle}</Button>
