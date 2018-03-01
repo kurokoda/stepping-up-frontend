b31a28107fcf5ace8b2336e139768a304b6afd57
Kurokoda <indigo.developer@gmail.com>
2018-03-01 01:04:44 -0500

    Updating auth pages

--- src/_web/views/content/auth/login/index.js
+++ src/_web/views/content/auth/login/index.js
@@ -1,17 +1,93 @@
-import React from 'react';
+import update from 'immutability-helper';
+import _ from 'lodash';
+import React, {Component} from 'react';
+import {Button} from 'react-bootstrap';
 import {withRouter} from 'react-router-dom';
+import {PasswordInput, TextInput} from '../../../../../shared/inputs';
+import Validation from '../../../../../shared/validation';
+import {minLength, required} from '../../../../../shared/validation/rules';
 
-const LoginView = () => {
-  return (<section style={styles.container}>
-    <div style={styles.content}>
-      LoginView
-    </div>
-  </section>);
-};
+class LoginView extends Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.onChange     = this.onChange.bind(this);
 
-// Exports -------------------------------------------------------------
+    this.fieldValidations = [
+      Validation.set('username', 'Username', required, minLength(3)),
+      Validation.set('password', 'Password', required)
+    ];
 
-export default withRouter(LoginView);
+    this.state = {
+      showErrors      : false,
+      validationErrors: {},
+      username        : '',
+      password        : '',
+    };
+  }
+
+  componentWillMount() {
+    this.setState({
+      validationErrors: Validation.run(this.state, this.fieldValidations)
+    });
+  }
+
+  render() {
+    return (
+      <div style={styles.container}>
+        <div>Template Component</div>
+        <form onSubmit={this.handleSubmit}>
+          <TextInput value={this.state.username}
+                     onChange={this.onChange}
+                     showError={this.state.showErrors}
+                     errorText={this.getErrorFor('username')}
+                     label='Username'
+                     id='username'/>
+
+          <PasswordInput value={this.state.password}
+                         onChange={this.onChange}
+                         showError={this.state.showErrors}
+                         errorText={this.getErrorFor('password')}
+                         label='Password'
+                         id='password'/>
+          <p>
+            <Button
+              className="btn-block"
+              bsStyle='success'
+              type='submit'>
+              LOG IN
+            </Button>
+          </p>
+        </form>
+      </div>
+    );
+  }
+
+  getErrorFor(field) {
+    return this.state.validationErrors[field];
+  }
+
+  onChange(event) {
+    event.preventDefault();
+    let newState              = update(this.state, {
+      [event.target.id]: {$set: event.target.value}
+    });
+    newState.validationErrors = Validation.run(newState, this.fieldValidations);
+    this.setState(newState);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState({showErrors: true});
+    if (!_.isEmpty(this.state.validationErrors)) return null;
+    this.props.onSubmit(
+      {
+        username: this.state.username,
+        password: this.state.password
+      }
+    );
+  }
+}
 
 LoginView.propTypes = {};
 
@@ -23,3 +99,6 @@ const styles = {
   container: {},
   content  : {},
 };
+
+export default withRouter(LoginView);
+
