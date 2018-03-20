35edc0cdf8944b53016ba14643800e4b1ed2fce1
Kurokoda <indigo.developer@gmail.com>
2018-03-20 14:57:27 -0400

    Updates defaut url for BE calls

--- src/shared/config/index.js
+++ src/shared/config/index.js
@@ -1,5 +1,5 @@
 export default {
-  API_BASE_URL  : 'http://localhost:5000', //process.env.REACT_APP_REACT_WEB_EXPRESS_API_BASE_URL || 'https://stepping-up-backend.herokuapp.com',
+  API_BASE_URL  : process.env.REACT_APP_REACT_WEB_EXPRESS_API_BASE_URL || 'http://localhost:5000',
   SHOW_DEV_TOOLS: process.env.REACT_APP_REACT_WEB_EXPRESS_SHOW_DEV_TOOLS || false,
   API_TYPE      : process.env.API_TYPE || 'mongoose',
 };
